0872a63aedfc36670172eb4a91e5aa2fa1d4acff
sugamasao <sugamasao@gmail.com>
2018-04-17 00:48:01 +0900

    http server sample

---
+++ http_server_01/app.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Path -> [%s]\n", r.URL.Path)
+  fmt.Fprintf(w, "Proto -> [%s]\n", r.Proto)
+  fmt.Fprintf(w, "Host -> [%s]\n", r.Host)
+  for k, v := range r.Header { 
+    fmt.Fprintf(w, "Header[%s] -> %s\n", k, v)
+  }
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe(":8080", nil)
+}
