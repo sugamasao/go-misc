efec6eb51df620bc619f684027b57d175e6a25c8
sugamasao <sugamasao@gmail.com>
2018-04-17 00:49:33 +0900

    fmt

--- http_server_01/app.go
+++ http_server_01/app.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Path -> [%s]\n", r.URL.Path)
-  fmt.Fprintf(w, "Proto -> [%s]\n", r.Proto)
-  fmt.Fprintf(w, "Host -> [%s]\n", r.Host)
-  for k, v := range r.Header { 
-    fmt.Fprintf(w, "Header[%s] -> %s\n", k, v)
-  }
+	fmt.Fprintf(w, "Path -> [%s]\n", r.URL.Path)
+	fmt.Fprintf(w, "Proto -> [%s]\n", r.Proto)
+	fmt.Fprintf(w, "Host -> [%s]\n", r.Host)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%s] -> %s\n", k, v)
+	}
 }
 
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
